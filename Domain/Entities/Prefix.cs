47b938940e1dd31404c2132f0579f55a9e28c666
AlteaQoku25 <altea.qoku@cardoai.com>
2023-07-11 17:01:34 +0200

    Data seed for Prefix Entity

--- Domain/Entities/Prefix.cs
+++ Domain/Entities/Prefix.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
     public class Prefix
     {
-        public int Id { get; set; }
-        public string Country_Prefix { get; set; }
-        public string Country_Name { get; set; }
+        
+            public int Id { get; set; }
+
+            [JsonPropertyName("code")]
+            public string Country_Code { get; set; }
+
+            [JsonPropertyName("prefix")]
+            public string Country_Prefix { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Country_Name { get; set; }
+        
     }
 }
