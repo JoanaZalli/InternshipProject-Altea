5b4115b001d1f5cf637e11e78d10162d6a0a15ed
AlteaQoku25 <altea.qoku@cardoai.com>
2023-07-13 09:26:49 +0200

    Data Seed for Role.

---
+++ Infrastructure/SeedData.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class SeedData
+    {
+        public static void Initizlize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()
+                ))
+            {
+                if (context.Prefixes.Any())
+                {
+                    return;
+                }
+                var env= serviceProvider.GetRequiredService<IWebHostEnvironment>();
+                var path = Path.Combine(env.WebRootPath, "Files", "prefixes.json");
+
+                var jsonString=System.IO.File.ReadAllText(path);
+                if(jsonString != null)
+                {
+#pragma warning disable CS8600
+                    var prefixes = System.Text.Json.JsonSerializer.Deserialize<List<Prefix>>(jsonString);
+#pragma warning restore CS8600
+
+                    if (prefixes != null)
+                    {
+                        context.Prefixes.AddRange(prefixes);
+                        context.SaveChanges();
+                    }
+
+                }
+            }
+        }
+    }
+}
